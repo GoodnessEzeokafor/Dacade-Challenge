ce83d95d75daaf7cf106e549799c77b3664b3136
Ezeokafor <gootech442@gmail.com>
2019-10-24 00:19:44 +0100

    Made changes
    Made changes

--- js/index.js
+++ js/index.js
@@ -113,8 +113,7 @@ $('#addBucketListBtn').click(async function(){
   const new_bucketlist = ($('#bucketlist').val());
   console.log("-------------------------------------")
   console.log("Val:",new_bucketlist)
-  const shit = await contractCall('add_new_bucketlist', [new_bucketlist],0);
-  console.log("SAVED TO THE DB", shit)
+  await contractCall('add_new_bucketlist', [new_bucketlist],0);
   bucketlistArr.push({
     index_counter: bucketlistLength.length + 1,
     bucketlist: new_bucketlist,
