baaa4e6a459c6269b16ad955f72069d2e8c9f96d
Ezeokafor <gootech442@gmail.com>
2019-10-24 00:14:07 +0100

    Fixed the call contract for the add bucketlist
    Fixed the call contract for the add bucketlist

--- js/index.js
+++ js/index.js
@@ -113,7 +113,7 @@ $('#addBucketListBtn').click(async function(){
   const new_bucketlist = ($('#bucketlist').val());
   console.log("-------------------------------------")
   console.log("Val:",new_bucketlist)
-  const shit = await contractCall('add_new_bucketlist', new_bucketlist,0);
+  const shit = await contractCall('add_new_bucketlist', [new_bucketlist],0);
   console.log("SAVED TO THE DB", shit)
   bucketlistArr.push({
     index_counter: bucketlistLength.length + 1,
